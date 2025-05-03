2359705d18e5a51dcfa588b56b638bb4663b2217
Ailin410 <ailinvanesa398@gmail.com>
2025-05-02 22:37:59 -0300

    Paso ocho: Creación de función

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -18,3 +18,7 @@ func _ready():
 func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = str(instancesCount)
+
+func delete():
+	instancesCount = 0
+	$CountLabel.text = str(instancesCount)
