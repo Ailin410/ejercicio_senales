4de1cd7022832a141381417cc79623424d8858ba
Ailin410 <ailinvanesa398@gmail.com>
2025-05-02 22:36:27 -0300

    Paso siete: Instanciar y llamar a la señal

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,6 +8,10 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	var block_red = get_node("RedGenerator")
+	block_red.button_down.connect(count_new_instance)
+	var block_violet = get_node("VioletGenerator")
+	block_violet.button.down.connect(count_new_instance)
 	pass
 
 
